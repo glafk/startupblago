0ae5bd6ccdc839a633628738e948a20725ca643a
Dimiter <dgkavinkoff@fmail.com>
2019-04-04 00:58:22 +0300

    Fixed db access

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   //db: 'mongodb://localhost/startup',
-  db: 'mongodb://<dbuser>:<dbpassword>@ds121716.mlab.com:21716/startup',
+  db: 'mongodb://glafk:Obi4amAzis@ds121716.mlab.com:21716/startup',
   port: process.env.PORT || 8080
 }
